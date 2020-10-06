1d27f5b44b70bb5a3e9007bf8f0be8a31c77062e
Anderson Braz de Sousa <braz_anderson@msn.com>
2020-10-06 08:51:42 -0300

    Up Files!

--- osworks-api/src/main/java/com/algaworks/osworks/api/controller/ContactController.java
+++ osworks-api/src/main/java/com/algaworks/osworks/api/controller/ContactController.java
@@ -4,28 +4,43 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Contact;
 import com.algaworks.osworks.domain.repository.ContactRepository;
 
 @RestController
+@RequestMapping("/contacts")
 public class ContactController {
 	
 	@Autowired
 	private ContactRepository contactRepository;
 
-	@GetMapping("/contacts")
+	@GetMapping
 	public List<Contact> listar() {
 		return contactRepository.findAll();
 	}
 	
-	@GetMapping("/contacts/{contactId}")
+	/*
+	@GetMapping("/{contactId}")
 	public Contact buscar(@PathVariable Long contactId) {
 		Optional<Contact> contact = contactRepository.findById(contactId);
 		return contact.orElse(null);
 	}
-
+	*/
+	
+	@GetMapping("/{contactId}")
+	public ResponseEntity<Contact> buscar(@PathVariable Long contactId) {
+		Optional<Contact> contact = contactRepository.findById(contactId);
+		
+		if(contact.isPresent()) {
+			return ResponseEntity.ok(contact.get());
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
 }
