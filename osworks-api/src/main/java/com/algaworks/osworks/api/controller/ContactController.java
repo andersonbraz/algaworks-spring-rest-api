e85fb76e1892cb52d701ccb0afaf7fdd94ca9fa4
Anderson Braz de Sousa <braz_anderson@msn.com>
2020-10-05 21:17:29 -0300

    Up Files

--- osworks-api/src/main/java/com/algaworks/osworks/api/controller/ContactController.java
+++ osworks-api/src/main/java/com/algaworks/osworks/api/controller/ContactController.java
@@ -6,8 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algaworks.domain.repository.ContactRepository;
 import com.algaworks.osworks.domain.model.Contact;
+import com.algaworks.osworks.domain.repository.ContactRepository;
 
 @RestController
 public class ContactController {
--- osworks-api/src/main/java/com/algaworks/osworks/domain/repository/ContactRepository.java
+++ osworks-api/src/main/java/com/algaworks/osworks/domain/repository/ContactRepository.java
@@ -1,4 +1,4 @@
-package com.algaworks.domain.repository;
+package com.algaworks.osworks.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
